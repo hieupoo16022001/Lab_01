c9d2c56c4c31d25b8dd3e408b72a499be80216aa
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-06-25 10:39:40 +0700

    lab 01

---
+++ session2/Snippet2.php
@@ -0,0 +1,5 @@
+<?php
+//THis is a single-line comment
+/*and this is a multi-line
+comment */
+?>
---
+++ session3/Snippet7.php
@@ -0,0 +1,19 @@
+<?php
+    namespace aptech;
+    class Boton{
+    function say(){
+        echo "Boston \n";
+    }
+    }
+    class NewYork{
+        function say(){
+            echo "NewYork \n";
+        }
+    }
+    function fool(){
+        echo"this is foo1() \n";
+    }
+    function fool2(){
+        echo "this is foo2() \n";
+    }
+    ?>
